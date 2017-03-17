a6f1ed6e2eb2ffaa25efe4e42ceb756c6c89e747
thj01 <thj001@gmail.com>
2017-03-17 11:16:25 +0100

    1

---
+++ mysql/php_programmer/hej_streng.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8" lang="da"/>
+<?php
+
+	$var1 = "hej ";
+	
+	$var2 = "verden";
+	
+	echo $var1.$var2;
+
+?>
+
---
+++ mysql/php_programmer/mail_recieve.php
@@ -0,0 +1,9 @@
+<meta charset="UTF-8" lang="da"/>
+
+<?php
+
+	$email = $_POST["email"];
+	
+	echo "Du sendte: ".$email;
+
+?>
