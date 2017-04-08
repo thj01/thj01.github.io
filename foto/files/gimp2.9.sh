4813f6ff5f137615b1b577990c2a0165aecba015
thj01 <thj001@gmail.com>
2017-04-08 12:55:13 +0200

    1

--- foto/files/gimp2.9.sh
+++ foto/files/gimp2.9.sh
@@ -1,5 +1,5 @@
 #Afinstaller GIMP 2.8 og installer 2.9
-sudo apt purge gimp -y
+sudo apt purge gimp gimp-lensfun gimp-plugin-registry gimp-texturize -y
 sudo apt autoremove -y
 sudo rm /etc/apt/sources.list.d/otto-kesselgulasch*
 sudo add-apt-repository ppa:otto-kesselgulasch/gimp-edge -y
