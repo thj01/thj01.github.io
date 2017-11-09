bda37370cc978b4756738f0484ff5d06d9dcae56
thj01 <thj001@gmail.com>
2017-11-09 09:36:12 +0100

    1

---
+++ python/programmer/bum.py
@@ -0,0 +1,10 @@
+tal = 20
+tabel = 3
+
+for i in range(1,tal+1):
+
+	if (i)%tabel==0:
+		print("BUM", end=" ")
+		
+	else:
+		print(i, end=" ")
---
+++ python/programmer/vinkelsum.py
@@ -0,0 +1,6 @@
+for i in range(3,20):
+	
+	vinkelsum = 180*(i-2) 
+	enkelt_vinkel = vinkelsum/i
+	
+	print("I en {0}-kant er vinkelsummen {1}° og den enkelte vinkel er {2}°".format(i,vinkelsum,enkelt_vinkel))
