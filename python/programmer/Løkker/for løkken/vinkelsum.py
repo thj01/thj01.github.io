8214b071f1f5cffbf29a46f5576cb97662a40e0a
thj01 <thj001@gmail.com>
2017-11-10 09:15:22 +0100

    1

--- python/programmer/Løkker/for løkken/vinkelsum.py
+++ python/programmer/Løkker/for løkken/vinkelsum.py
@@ -2,5 +2,12 @@ for i in range(3,20):
 	
 	vinkelsum = 180*(i-2) 
 	enkelt_vinkel = vinkelsum/i
+
+	enkelt_vinkel = round(enkelt_vinkel,2)
+		
+	if enkelt_vinkel.is_integer():
+		
+		enkelt_vinkel = int(enkelt_vinkel)
+		
 	
 	print("I en {0}-kant er vinkelsummen {1}° og den enkelte vinkel er {2}°".format(i,vinkelsum,enkelt_vinkel))
