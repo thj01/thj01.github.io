2810e302a489c7ab18528042124ffb1110f4d7ae
thj01 <thj001@gmail.com>
2017-11-10 09:02:22 +0100

    1

--- python/programmer/test/test.py
+++ python/programmer/test/test.py
@@ -1,1 +1,18 @@
+def bum(a,b):
+	
+	# a er tallene der skal undersøges
+	# b tabellen der skal undersøges
+	
+	for i in range(a):
+		
+		i +=1
+		
+		if i%b == 0:
+			
+			print("BUM")
 
+		else:
+			
+			print(i)
+
+bum(16,3)
