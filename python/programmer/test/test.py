f7dd4c4da4c967de823afb8d261fa6e5e85cfdfa
thj01 <thj001@gmail.com>
2017-11-14 14:14:24 +0100

    1

--- python/programmer/test/test.py
+++ python/programmer/test/test.py
@@ -1,1 +1,21 @@
-raise SystemExit
+from time import sleep
+
+while True:
+
+	for i in range(10,-1,-1):
+		
+		if i ==0:
+			print("Fyr")
+			exit()
+		
+		print(i)
+		sleep(1)
+		
+
+	
+
+	
+
+
+			
+			
