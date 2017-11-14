212a9fb6f09e5a41322aeb6c5871a7f2f6a179c6
thj01 <thj001@gmail.com>
2017-11-14 08:23:32 +0100

    1

--- python/programmer/test/test.py
+++ python/programmer/test/test.py
@@ -1,3 +1,12 @@
-brugere={"Anne": 1, "Bent": 2}
+brugerliste={}
 
-print(brugere)
+for i in range(3):
+	
+	brugernavn = input("\nHvad skal du bruger hedde?: ")
+	password = int(input("\n\tIndtast password (kun tal): "))
+	
+	brugerliste["{0}".format(brugernavn)] = password
+	
+	
+	
+print("\nHer er din brugerliste: {0}".format(brugerliste))
