c5e121fd6983c09cebb77ea488f07869e0279475
thj01 <thj001@gmail.com>
2017-11-20 12:06:30 +0100

    1

--- python/programmer/test/test.py
+++ python/programmer/test/test.py
@@ -1,21 +1,38 @@
-from time import sleep
+mail = input("Indtast din mail: ")
 
-while True:
+mail_len = len(mail)
 
-	for i in range(10,-1,-1):
-		
-		if i ==0:
-			print("Fyr")
-			exit()
-		
-		print(i)
-		sleep(1)
-		
-
+count_a = mail.count("@")
 	
+count_a_last = mail[-4:].count("@")
 
+fejl = 0
 	
+if mail_len<7:
+	
+	print("Mailen har for få tegn (mindre end 7)")
+	
+	fejl +=1
+	
+if count_a < 1:
+	
+	print("Mailen mangler @")
+	
+	fejl +=1
+	
+if count_a > 1:
+	
+	print("Mailen har for mange @")
+	
+	fejl +=1
 
-
+if count_a_last > 1:
+	
+	print("@ må ikke stå blandt de sidste 4 tegn.")
+	
+	fejl +=1
 			
 			
+if fejl == 0:
+	
+	print("mailen er konstrueret korrekt ")
