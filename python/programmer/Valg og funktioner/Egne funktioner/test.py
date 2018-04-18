4763d9f3b667ee0581a5fabd35b4f1814e28d016
thj01 <thj001@gmail.com>
2018-04-18 10:22:16 +0200

    1

--- python/programmer/Valg og funktioner/Egne funktioner/test.py
+++ python/programmer/Valg og funktioner/Egne funktioner/test.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-kategorier = ["gruppe a", "gruppe b", "gruppe c", "gruppe d"]
-værdier = [5, 20, 17, 12]
-
-plt.plot(kategorier, værdier, "ro", markersize=5)
-plt.title('Prik diagram')
+textstr = 'NSE=%.2f\nRMSE=%.2f\n'%(1, 2)
+plt.xlim(2002, 2008)
+plt.ylim(0, 4500)
+# print textstr
+plt.text(2000, 2000, textstr, fontsize=14)
+plt.grid(True)
+plt.subplots_adjust(right=.5)
 plt.show()
