26ed16b4e84fcbf6f0ef1fa4d4abfe93cbf5c9c1
thj01 <thj001@gmail.com>
2018-04-25 09:12:13 +0200

    1

--- python/programmer/Valg og funktioner/Egne funktioner/test.py
+++ python/programmer/Valg og funktioner/Egne funktioner/test.py
@@ -1,29 +1,4 @@
-<<<<<<< HEAD
-import tkinter as tk
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
-
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=root.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
-
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
-=======
 import random
 
 enkelt_ord = "aften"
@@ -68,4 +43,3 @@ for i in liste:
 
 print(liste_rest)
 
->>>>>>> 15ead93df63af4b6e1a3123078976cb672c0c259
