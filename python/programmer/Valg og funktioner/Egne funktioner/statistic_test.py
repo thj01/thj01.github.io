cd2313a61d096fadfc4b75e216f30e5ed9336452
thj01 <thj001@gmail.com>
2018-05-02 13:52:55 +0200

    1

--- python/programmer/Valg og funktioner/Egne funktioner/statistic_test.py
+++ python/programmer/Valg og funktioner/Egne funktioner/statistic_test.py
@@ -1,15 +1,1 @@
-import statistics as st
-import numpy as np
-import matplotlib.pyplot as plt
-import csv
-
-with open("karakterer.csv", "r") as csvfile:
-		reader = csv.reader(csvfile)
-		x = list(reader)
-
-print(x)
-results = ["1", "2", "3"]
-print(results)
-results = [int(i) for i in results]
-print(results)
-[1, 2, 3]
+int("k")
