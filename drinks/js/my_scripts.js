ecc48c61238feddb75a5c149fb6d552bb6ca2be7
tj <thj001@gmail.com>
2022-08-27 16:35:38 +0200

    2022-08-27

--- drinks/js/my_scripts.js
+++ drinks/js/my_scripts.js
@@ -9,11 +9,3 @@ function loadDoc(doc) {
   xhttp.open("GET", doc , true);
   xhttp.send();
 }
-
-const navLinks = document.querySelectorAll('.nav-item');
-const menuToggle = document.querySelector('#offcanvasNavbar');
-navLinks.forEach((elem) => {
-  elem.addEventListener('click', () => {
-    $("button.navbar-toggler").click();
-  });
-});
