37ccbd601e50d03a07d7735cb711256da00d4728
tj <thj001@gmail.com>
2023-12-13 20:44:18 +0100

    2023-12-13

--- dice/js/my_scripts.js
+++ dice/js/my_scripts.js
@@ -10,6 +10,8 @@ function loadDoc(doc) {
   xhttp.send();
 }
 
+var sides = 6
+
 var dice = {
   roll: function () {
     var randomNumber = Math.floor(Math.random() * sides) + 1;
@@ -18,52 +20,61 @@ var dice = {
 }
 
 
+function change_background_color() {
+  document.getElementById("p_dice").style.backgroundColor = "lightblue";
 
-//Prints dice roll to the page
 
-function printNumber(number) {
-  var placeholder = document.getElementById('Dice_roll');
-  placeholder.innerHTML = number;
+}
+
+
+
+function all_dices(d0,d1,d2){
+
+  var diceroll00 = document.getElementById('dice00');
+  var diceroll01 = document.getElementById('dice01');
+  var diceroll02 = document.getElementById('dice02');
+  diceroll00.innerHTML = d0;
+  diceroll01.innerHTML = d1;
+  diceroll02.innerHTML = d2;
+
+
 }
 
 var button = document.getElementById('button');
 
 button.onclick = function() {
-  var result = dice.roll();
-  printNumber(result);
-};
+  // var result = dice.roll();
+  // printNumber(result);
+  var dice00 = dice.roll();
+  var dice01 = dice.roll();
+  var dice02 = dice.roll();
+  all_dices(dice00,dice01,dice02)
+
+}
 
 function new_dice_type(new_sides_var){
-  var sides = new_sides_var
+  var sides = new_sides_var;
 
 }
 
 function test(n){
 
-  
+  sides = n;
 
   if (n != 2) {
 
     document.getElementById('dice_type').innerHTML = n;
     document.getElementById('btn_dice_type').innerHTML = 'D' + n;
-    document.getElementById('Dice_roll').innerHTML = '?';
+    document.getElementById('Dice_roll').innerHTML = 1;
 
   } else {
     document.getElementById('dice_type').innerHTML = n;
     document.getElementById('btn_dice_type').innerHTML = 'Møntkast';
-    document.getElementById('Dice_roll').innerHTML = '?';
-
+    document.getElementById('Dice_roll').innerHTML = 1;
 
   }
 
-
-
   
-  
-  
-  sides = n
 
 }
 
-
-var sides = 6
