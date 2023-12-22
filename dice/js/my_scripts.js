15bc574ad6d3fd75475cd75377160b0ab4531bbd
tj <thj001@gmail.com>
2023-12-22 14:02:24 +0100

    2023-12-22

--- dice/js/my_scripts.js
+++ dice/js/my_scripts.js
@@ -14,6 +14,7 @@ function loadDoc(doc) {
   click_counter_dice02 = 0;
   click_counter_dice03 = 0;
   click_counter_dice04 = 0;
+  click_counter_dice05 = 0;
 
   throw_counter = 0;
   document.getElementById("ThrowCount").innerHTML = throw_counter;
@@ -22,11 +23,15 @@ function loadDoc(doc) {
 
 var sides = 6;
 var throw_counter = 0;
+
+const dices = ["dice00","dice01","dice02","dice03","dice04","dice05"]
+
 var click_counter_dice00 = 0;
 var click_counter_dice01 = 0;
 var click_counter_dice02 = 0;
 var click_counter_dice03 = 0;
 var click_counter_dice04 = 0;
+var click_counter_dice05 = 0;
 
 var dice_unchosen = "Ivory";
 var dice_chosen = "lightblue";
@@ -80,18 +85,27 @@ function change_background_color(param) {
           document.getElementById(param).style.backgroundColor = dice_unchosen;
           click_counter_dice04 +=1
         }
+      } else if (param == "dice05"){ 
+        if (click_counter_dice05%2 == 0 ) {  
+          document.getElementById(param).style.backgroundColor = dice_chosen;
+          click_counter_dice05 +=1
+        } else {
+          document.getElementById(param).style.backgroundColor = dice_unchosen;
+          click_counter_dice05 +=1
+        }
       }
   }
 
 
 
-function all_dices(d0,d1,d2,d3,d4){
+function all_dices(d0,d1,d2,d3,d4,d5){
 
   var diceroll00 = document.getElementById('dice00');
   var diceroll01 = document.getElementById('dice01');
   var diceroll02 = document.getElementById('dice02');
   var diceroll03 = document.getElementById('dice03');
   var diceroll04 = document.getElementById('dice04');
+  var diceroll05 = document.getElementById('dice05');
   
   if (click_counter_dice00%2 == 0 ){
     diceroll00.innerHTML = d0;}
@@ -102,7 +116,9 @@ function all_dices(d0,d1,d2,d3,d4){
   if (click_counter_dice03%2 == 0 ){
     diceroll03.innerHTML = d3;}
   if (click_counter_dice04%2 == 0 ){
-    diceroll04.innerHTML = d4;}        
+    diceroll04.innerHTML = d4;}
+  if (click_counter_dice05%2 == 0 ){
+    diceroll05.innerHTML = d5;}          
 
 }
 
@@ -115,18 +131,18 @@ button.onclick = function() {
   document.getElementById("ThrowCount").innerHTML = throw_counter;
   
   var result00 = dice.roll();
-  var result00 = dice.roll();
   var result01 = dice.roll();
   var result02 = dice.roll();
   var result03 = dice.roll();
   var result04 = dice.roll();
-  all_dices(result00,result01,result02,result03,result04);
+  var result05 = dice.roll();
+  all_dices(result00,result01,result02,result03,result04,result05);
 
 }
 
 var reset_button = document.getElementById('reset_button');
 
-reset_button.onclick = function() {
+reset_button.onclick = function clear_data() {
 
   throw_counter = 0;
   document.getElementById("ThrowCount").innerHTML = throw_counter;
@@ -136,8 +152,7 @@ reset_button.onclick = function() {
   click_counter_dice02 = 0;
   click_counter_dice03 = 0;
   click_counter_dice04 = 0;
-
-  const dices = ["dice00","dice01","dice02","dice03","dice04"]
+  click_counter_dice05 = 0;
 
   for (const i of dices){
     document.getElementById(i).style.backgroundColor = dice_unchosen;  
@@ -145,41 +160,78 @@ reset_button.onclick = function() {
 
   }
 
-
-
-  // document.getElementById("dice00").style.backgroundColor = dice_unchosen;
-  // document.getElementById("dice01").style.backgroundColor = dice_unchosen;
-  // document.getElementById("dice02").style.backgroundColor = dice_unchosen;
-  // document.getElementById("dice03").style.backgroundColor = dice_unchosen;
-  // document.getElementById("dice04").style.backgroundColor = dice_unchosen;
-
 }
 
 
 
-function new_dice_type(new_sides_var){
-  var sides = new_sides_var;
+
+
+// function new_dice_type(new_sides_var){
+//   var sides = new_sides_var;
+
+// }
+
+var change_to_coin = document.getElementById('design_as_coin');
+
+change_to_coin.onclick = function () {
+
+  for (const i of dices){
+    document.getElementById(i).style.borderRadius = ".9em";
+  }
+ 
+  sides = 2;
 
 }
+  
+// var change_to_dice = document.getElementsByClassName("design_as_dice")
+
+// change_to_dice.onclick = function () {
+
+//   for (const i of dices){
+//     document.getElementById(i).style.borderRadius = ".9em";
+//   }
+
+//   test(2)
+// }
+
+  // for (const i of dices){
+  //   document.getElementById(i).style.borderRadius = ".9em";  
+  //   }
+
+
+
 
 function test(n){
 
+
+
   sides = n;
+
 
   if (n != 2) {
 
-    document.getElementById('dice_type').innerHTML = n;
+    document.getElementById('dice_type').innerHTML = 'DICE - D' + n;
     document.getElementById('btn_dice_type').innerHTML = 'D' + n;
     document.getElementById('Dice_roll').innerHTML = 1;
 
+    for (const i of dices){document.getElementById(i).innerHTML = "&#9733;"}
+
+
+
   } else {
-    document.getElementById('dice_type').innerHTML = n;
+    document.getElementById('dice_type').innerHTML = 'Møntkast';
     document.getElementById('btn_dice_type').innerHTML = 'Møntkast';
     document.getElementById('Dice_roll').innerHTML = 1;
 
+    // for (const i of dices){
+    //   document.getElementById(i).style.borderRadius = ".9em";
+
+    sides = 2;
   }
 
-  
+
 
 }
+
+
 
