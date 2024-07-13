26fe1b415328cf58355cae96ec7cca84ffbaf0bc
tj <thj001@gmail.com>
2024-07-13 09:40:08 +0200

    2024-07-13

---
+++ Kink_app/js/my_scripts.js
@@ -0,0 +1,16 @@
+
+
+function loadDoc(doc) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("content").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", doc , true);
+  xhttp.send();
+
+
+}
+
