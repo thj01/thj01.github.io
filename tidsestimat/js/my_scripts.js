cd59d4c2abc5f5c4b32355550cc72d31182ff5d4
tj <thj001@gmail.com>
2025-01-19 18:51:05 +0100

    2025-01-19

--- tidsestimat/js/my_scripts.js
+++ tidsestimat/js/my_scripts.js
@@ -16,52 +16,54 @@ function loadDoc(doc) {
 
 var test = 0;
 
+  // til test //
+
 
 
 
 function beregn() {
-	
+
   var Vo = parseFloat(document.getElementById('optimistisk_tidsforbrug').value);
   var Vs = parseFloat(document.getElementById('sandsynligt_tidsforbrug').value);
   var Vp = parseFloat(document.getElementById('pessimistisk_tidsforbrug').value);
   var Ea = parseFloat(document.getElementById('effektiv_arbejdstid').value);
-  
-  // til test //
 
   if (test == 1){
-  Vo = 1;
-  Vs = 3;
-  Vp = 4;
-  Ea = 6;
-  };
-
-
+    Vo = 1;
+    Vs = 3;
+    Vp = 4;
+    Ea = 6;
+    };
+  
   var Vm = (Vo + Vs*2.95 + Vp)/4.95;
   var s_2 = Math.pow((Vp-Vo),2)/21.66;
   var s = Math.pow(s_2,1/2);
-  // var Vm
-  var kontrol = s/Vm*20;
+  var kontrol = s/Vm;
 
+  
   console.log("Vm: "+Vm );
   console.log("s²: " + s_2);
   console.log("s: " + s);
   console.log("Kontrol-tal: " + kontrol);
   
 
-  // kontrol = 2.3;
-
-  
-
   document.getElementById("kontrol").innerHTML = kontrol.toFixed(4);
-  document.getElementById("kontrol_evaluate").innerHTML = kontrol.toFixed(1);
+  document.getElementById("kontrol_evaluate").innerHTML = kontrol.toFixed(2);
 
   // https://stackoverflow.com/questions/195951/how-can-i-change-an-elements-class-with-javascript
 
   var change_kontrol_box_color = document.getElementById("kontrol_box_color");
 
-  var kontrol_jumps = [0,1,2,3]
+  var vurderingsfaktor = 20;
+  
+  var kontrol_jumps = [
+    0.0, // Neder
+    1/vurderingsfaktor,
+    2/vurderingsfaktor,
+    3/vurderingsfaktor
+  ]
 
-  if (kontrol > kontrol_jumps[0]) {
+  if (kontrol >= kontrol_jumps[0]) {
     change_kontrol_box_color.className = "bg-success";
     change_kontrol_box_color.classList += " text-white";
   }
@@ -76,48 +78,6 @@ function beregn() {
   if (kontrol > kontrol_jumps[3]) {
     change_kontrol_box_color.className = "bg-danger";
   }
-
-
-
-  // if (kontrol < 1) {
-  //   change_alert_type.className = "alert";
-  //   change_alert_type.classList.add("alert-succes");
-  //   // change_alert_type.classList.add("alert-succes");
-  //   change_alert_type.classList.add("text-center");
-
-  //   // document.getElementById("alert_color").classList.add("alert alert-succes text-center");
-  // }
-
-
-  // if (kontrol < 4) {class_element.className = "alert alert-danger text-center";}
-  // if (kontrol < 3) {class_element.className = "alert alert-warning text-center";}
-  // if (kontrol < 2) {class_element.className = "alert alert-primary text-center";}
-  // if (kontrol < 1) {
-  //   document.getElementById("alert_color").className = "alert";
-  //   // document.getElementById("alert_color").classlist.add(" alert-light text-center");
-  // }
-
-
-  // if (kontrol < 2) {document.getElementById("alert_color").className = "alert alert-primary text-center";}
-  // if (kontrol < 3) {document.getElementById("alert_color").className = "alert alert-warning text-center";}
-  // if (kontrol < 4) {document.getElementById("alert_color").className = "alert alert-danger text-center";}
-
-
-
-  // switch(kontrol) {
-
-  //   case (kontrol < 1.2):
-  //     document.getElementById("alert_color").className = "alert alert-primary text-center";
-  //     break;
-  //   case (kontrol >= 1.2):
-  //     document.getElementById("alert_color").className = "alert alert-danger text-center";
-  //     break;
-
-
-
-
-  // };
-
 
 
   var succes68_lav = (Vm-s).toFixed(1);
@@ -142,12 +102,27 @@ document.getElementById('s').innerHTML = s.toFixed(4);
 // document.getElementById('gns_lav').innerHTML = Vm.toFixed(1);
 // document.getElementById('gns_høj').innerHTML = Vm.toFixed(1);
 document.getElementById('gns_timer').innerHTML = Vm.toFixed(1);
+document.getElementById('Vm_68').innerHTML = Vm.toFixed(1);
+document.getElementById('s_68').innerHTML = 1*parseFloat(s).toFixed(2);
+document.getElementById('Vm_95').innerHTML = Vm.toFixed(1);
+document.getElementById('s_95').innerHTML = 2*parseFloat(s).toFixed(2);
+document.getElementById('Vm_99').innerHTML = Vm.toFixed(1);
+document.getElementById('s_99').innerHTML = 3*parseFloat(s).toFixed(2);
+
+
+// document.getElementById('Vm_99').innerHTML = Vm.toFixed(1);
 document.getElementById('succes68_lav').innerHTML = succes68_lav;
 document.getElementById('succes68_høj').innerHTML = succes68_høj;
 document.getElementById('succes95_lav').innerHTML = succes95_lav;
 document.getElementById('succes95_høj').innerHTML = succes95_høj;
 document.getElementById('succes99_lav').innerHTML = succes99_lav;
 document.getElementById('succes99_høj').innerHTML = succes99_høj;
+// document.getElementById('succes68_lav_graphics').innerHTML = succes68_lav + " timer";
+// document.getElementById('succes68_høj_graphics').innerHTML = succes68_høj + " timer";
+// document.getElementById('succes95_lav_graphics').innerHTML = succes95_lav;
+// document.getElementById('succes95_høj_graphics').innerHTML = succes95_høj;
+// document.getElementById('succes99_lav_graphics').innerHTML = succes99_lav;
+// document.getElementById('succes99_høj_graphics').innerHTML = succes99_høj;
 
 // document.getElementById('gns_lav_dage').innerHTML = (Vm/Ea).toFixed(1);
 // document.getElementById('gns_høj_dage').innerHTML = (Vm/Ea).toFixed(1);
@@ -163,133 +138,128 @@ document.getElementById('succes99_høj_dage').innerHTML = (succes99_høj/Ea).toF
 }
 
 function showDiv() {
- document.getElementById('showoff').style.display = "block";
-}
+ document.getElementById('result').style.display = "block";
+};
 
-new Chart("myChart", {
-  type: "line",
-  data: {
-    // labels: xValues,
-    datasets: [{ 
-      label: "graph",
-      data: [
-        {x:0,y:0},
-        {x:5,y:1},
-        {x:10,y:4},
-        {x:15,y:10},
-        {x:20,y:16},
-        {x:25,y:21},
-        {x:30,y:24},
-        {x:35,y:25},
-        {x:40,y:24},
-        {x:45,y:22},
-        {x:50,y:19},
-        {x:55,y:15},
-        {x:60,y:11},
-        {x:65,y:8},
-        {x:70,y:5.8},
-        {x:75,y:4.2},
-        {x:80,y:3},        
-        {x:85,y:1.8},
-        {x:90,y:1},
-        {x:95,y:.65},
-        {x:100,y:.5},
-      ],
-      borderColor: "black",
-      borderWidth: 1,
-      fill: false
+function show_grunddata() {
+  document.getElementById('grunddata').style.display = "block";
+ };
+ 
 
-    }, 
-    // { 
-    //   label: "højdebrgrænser",
-    //   data: [
-    //     {x:0,y:33},
-    //   ],
-    //   borderColor: "red",
-    //   borderWidth: .5,
-    //   fill: false,
+// new Chart("myChart", {
+//   type: "line",
+//   data: {
+//     // labels: xValues,
+//     datasets: [{ 
+//       label: "graph",
+//       data: [
+//         {x:0,y:0},
+//         {x:5,y:1},
+//         {x:10,y:4},
+//         {x:15,y:10},
+//         {x:20,y:16},
+//         {x:25,y:21},
+//         {x:30,y:24},
+//         {x:35,y:25},
+//         {x:40,y:24},
+//         {x:45,y:22},
+//         {x:50,y:19},
+//         {x:55,y:15},
+//         {x:60,y:11},
+//         {x:65,y:8},
+//         {x:70,y:5.8},
+//         {x:75,y:4.2},
+//         {x:80,y:3},        
+//         {x:85,y:1.8},
+//         {x:90,y:1},
+//         {x:95,y:.65},
+//         {x:100,y:.5},
+//       ],
+//       borderColor: "black",
+//       borderWidth: 1,
+//       fill: false
 
-    // }, 
-    { 
-      label: "Vo",
-      data: [
-        {x:5,y:-2},
-        {x:5,y:3},
-      ],
-      borderColor: "limegreen",
-      borderWidth:5,
-      fill: false,
+//     }, 
+//      { 
+//       label: "Vo",
+//       data: [
+//         {x:5,y:-2},
+//         {x:5,y:3},
+//       ],
+//       borderColor: "limegreen",
+//       borderWidth:5,
+//       fill: false,
 
-    }, { 
-      label: "Vs",
-      data: [
-        {x:35,y:-2},
-        {x:35,y:28},
-      ],
-      borderColor: "DodgerBlue ",
-      borderWidth: 5,
-      fill: false,
+//     }, { 
+//       label: "Vs",
+//       data: [
+//         {x:35,y:-2},
+//         {x:35,y:28},
+//       ],
+//       borderColor: "DodgerBlue ",
+//       borderWidth: 5,
+//       fill: false,
 
-    }, { 
-      label: "Vm",
-      data: [
-        {x:45,y:-2},
-        {x:45,y:25},
-      ],
-      borderColor: "Gold ",
-      borderWidth: 5,
-      fill: false,
+//     }, { 
+//       label: "Vm",
+//       data: [
+//         {x:45,y:-2},
+//         {x:45,y:25},
+//       ],
+//       borderColor: "Gold ",
+//       borderWidth: 5,
+//       fill: false,
 
-    },  { 
-      label: "Vp",
-      data: [
-        {x:92,y:-2},
-        {x:92,y:3},
-      ],
-      borderColor: "red",
-      borderWidth: 5,
-      fill: false,
+//     },  { 
+//       label: "Vp",
+//       data: [
+//         {x:92,y:-2},
+//         {x:92,y:3},
+//       ],
+//       borderColor: "red",
+//       borderWidth: 5,
+//       fill: false,
 
-    }]
+//     }]
 
-  },
+//   },
   
 
-  options: {
-    // legend: {display: false},
-    elements: {
-      point:{radius: 0}
-    },
-    scales: {
-      xAxes: [{
-          type: 'linear',
-          position: 'bottom',
-          display: true,
-          ticks: {
-            display:false,
-          },
-          // gridLines: {
-          //   display:false,
-          // },
-      }],
-      yAxes: [{
-        type: "linear",
-        position: "left",
-        display: true,
-        ticks: {
-          display:false,
-        },
-        // gridLines: {
-        //   display:false,
-        // },
-      }],
-      // y: {
-      //   suggestedMin: 50,
-      //   suggestedMax: 100
-      // },
-    // datalabels: {display: false,}
-    }
-  }
+//   options: {
+//     // legend: {display: false},
+//     elements: {
+//       point:{radius: 0}
+//     },
+//     scales: {
+//       xAxes: [{
+//           type: 'linear',
+//           position: 'bottom',
+//           display: true,
+//           ticks: {
+//             display:false,
+//           },
+//           // gridLines: {
+//           //   display:false,
+//           // },
+//       }],
+//       yAxes: [{
+//         type: "linear",
+//         position: "left",
+//         display: true,
+//         ticks: {
+//           display:false,
+//         },
+//         // gridLines: {
+//         //   display:false,
+//         // },
+//       }],
+//       // y: {
+//       //   suggestedMin: 50,
+//       //   suggestedMax: 100
+//       // },
+//     // datalabels: {display: false,}
+//     }
+//   }
 
-});
+// });
 
